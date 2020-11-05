74fd37bbac93a0ff8103c64a38958f502e17dee2
sc39 <44244999+sc39IsADev@users.noreply.github.com>
2020-11-05 16:06:48 -0500

    Update install.py

--- install.py
+++ install.py
@@ -11,10 +11,12 @@ stop-twm
 stop-mwm
 """
 WSLKIT_S1 = """
+export DISPLAY=:0.0
 twm &
 xterm &
 """
 WSLKIT_S2 = """
+export DISPLAY=:0.0
 mwm &
 xterm &
 """
@@ -30,17 +32,10 @@ print(WSLKIT_L1)
 print(WSLKIT_L2)
 os.system("apt install git xterm x11-apps mwm twm build-essential autoconf automake mesa-utils crispy-doom emacs xemacs21 wget curl")
 os.system("apt update")
-bashrc = open(homedir + "/.bashrc", "a")
-bashrc.write("\nexport DISPLAY=:0.0\nexport PATH=$PATH:/wslkit")
-bashrc.close()
-try:
-	os.mkdir("/wslkit")
-except:
-	pass
-starttwm = open("/wslkit/start-twm", "w")
+starttwm = open("/usr/bin/start-twm", "w")
 starttwm.write(WSLKIT_S1)
 starttwm.close()
-startmwm = open("/wslkit/start-twm", "w")
+startmwm = open("/usr/bin/start-twm", "w")
 startmwm.write(WSLKIT_S1)
 startmwm.close()
 stoptwm = open("/wslkit/start-twm", "w")
